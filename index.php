89e0372d08d4e7d3ea2fbe987943856b6b70dae6
Sheikh Rafid Reza <rafid.reza.nabil@gmail.com>
2016-12-04 23:29:23 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+    <body>
+        <h2><center>Welcome to Rush &amp Rown</center></h2>
+        <br>
+        <br>
+        <br>
+        <p><a href="add_product.php"><center>Add Product</center></a></p>
+        <p><a href="update_product.php"><center>Update Product</center></a></p>
+        <p><a href="show_product.php"><center>Show Product</center></a></p>
+        <p><a href="#">Delete Product</a></p>
+        <p><a href="#">Check Remain Product</a></p>
+        <p><a href="#">Place Order</a></p>
+    </body>
+</html># project
